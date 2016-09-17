4dc194f453b0cd2e8b3920048718900a43d48967
Colin <colinnlsn@gmail.com>
2016-09-17 18:08:41 -0500

    Updated Readme And Index
    Updated the Readme as well as added the index.js code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # heights-facts
 Simple Alexa skill to vend facts about the Houston Heights
+
+Based off of the sample Alexa Space Facts skill
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var SKILL_NAME = 'Heights Facts';
+
+/**
+ * Array containing space facts.
+ */
+var FACTS = [
+    "The Heights is an awesome place",
+    "Onion Creek is a great place to eat.",
+    "Coultivare does not take reservations.",
+    "There are several places to do yoga in the Heights.",
+    "Stude Park is undergoing several renovations in 2016",
+    "The heights is home to some of the oldest houses in Houston",
+    "There are lots of Poke stops on Heights Boulevard",
+    "Most bars will give you a 10 % discount for biking up to them"
+];
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random space fact from the space facts list
+        var factIndex = Math.floor(Math.random() * FACTS.length);
+        var randomFact = FACTS[factIndex];
+
+        // Create speech output
+        var speechOutput = "Here's your fact: " + randomFact;
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
